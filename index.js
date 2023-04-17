0c323a874555c6218ccea0a2170a8ff63aeada76
sudharm jadhav <sudharmjadhav2004@gmail.com>
2023-04-17 01:56:16 +0000

    to select x-o on the cell added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let sign;
+let sign = null;
 console.log("1");
 
 const boxes = document.getElementsByClassName("box");
@@ -6,9 +6,22 @@ const boxes = document.getElementsByClassName("box");
 console.log(boxes[0]);
 console.log(boxes.length);
 
-// for (let index = 0; index < boxes.length; index++) {
-//   boxes[index].addEventListener("click", () => {
-//     console.log("hello");
-//   });
-//   console.log("what");
-// }
+for (let index = 0; index < boxes.length; index++) {
+  boxes[index].addEventListener("click", (event) => {
+    if (sign == null || sign == "x") {
+      event.target.classList.add("o-sign");
+      sign = "o";
+    } else {
+      event.target.classList.add("x-sign");
+      sign = "x";
+    }
+  });
+}
+
+const restartBtn = document.querySelector(".button");
+restartBtn.addEventListener("click", (event) => {
+  for (let index = 0; index < boxes.length; index++) {
+    boxes[index].classList.remove("o-sign");
+    boxes[index].classList.remove("x-sign");
+  }
+});
