c0021daa7fa0fc7eb4612dc187ca4198da46fb77
sudharm2004 <sudharmjadhav2004@gmail.com>
2023-04-17 20:21:30 +0530

    Add basic functionality for Tic Tac Toe game with pending logic for determining the winner

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 let sign = null;
-console.log("1");
 
-const boxes = document.getElementsByClassName("box");
+document.addEventListener("DOMContentLoaded", (documentEvent) => {
+  //getting parent div of restart button and making its display hide
+  const restartBtnDiv = document.querySelector(".restart-btn-container");
+  console.log(restartBtnDiv);
+  restartBtnDiv.classList.add("hide");
+  //getting the all boxes
+  const boxes = document.getElementsByClassName("box");
 
-console.log(boxes[0]);
-console.log(boxes.length);
-
-for (let index = 0; index < boxes.length; index++) {
-  boxes[index].addEventListener("click", (event) => {
-    if (sign == null || sign == "x") {
-      event.target.classList.add("o-sign");
-      sign = "o";
-    } else {
-      event.target.classList.add("x-sign");
-      sign = "x";
+  const handleBoxClick = (event) => {
+    //to check that clicked element does not have X as well as O
+    if (
+      !event.target.classList.contains("o-sign") &&
+      !event.target.classList.contains("x-sign")
+    ) {
+      //If after clicking on the tic-toc-toe box sign(variable that represent is it turn for X or O) is checked to decide current player(X or O) if previous value was x print o or else print x and also change value of sing
+      if (sign == null || sign == "x") {
+        event.target.classList.add("o-sign");
+        sign = "o";
+      } else {
+        event.target.classList.add("x-sign");
+        sign = "x";
+      }
+      restartBtnDiv.classList.remove("hide");
     }
-  });
-}
+  };
 
-const restartBtn = document.querySelector(".button");
-restartBtn.addEventListener("click", (event) => {
+  //applying onclick event on all classes selected above using for loop
   for (let index = 0; index < boxes.length; index++) {
-    boxes[index].classList.remove("o-sign");
-    boxes[index].classList.remove("x-sign");
+    //for each element
+    boxes[index].addEventListener("click", handleBoxClick);
   }
+
+  //Code for wining the game
+
+  //Code for restarting the game
+  const restartBtn = document.getElementById("restart-btn");
+
+  const restartGame = () => {
+    for (let index = 0; index < boxes.length; index++) {
+      boxes[index].classList.remove("o-sign");
+      boxes[index].classList.remove("x-sign");
+    }
+    restartBtnDiv.classList.add("hide");
+  };
+
+  restartBtn.addEventListener("click", restartGame);
 });
