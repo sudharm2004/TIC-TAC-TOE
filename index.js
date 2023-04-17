0666189f2db22206ce2cdd24682613087f01ec1e
sudharm2004 <sudharmjadhav2004@gmail.com>
2023-04-17 07:05:24 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let sign;
+console.log("1");
+
+const boxes = document.getElementsByClassName("box");
+
+console.log(boxes[0]);
+console.log(boxes.length);
+
+// for (let index = 0; index < boxes.length; index++) {
+//   boxes[index].addEventListener("click", () => {
+//     console.log("hello");
+//   });
+//   console.log("what");
+// }
